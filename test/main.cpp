c3486431ccf0d040098323c02998b5880aa798f8
michaelkotor <milan3020@gmail.com>
2021-03-13 22:35:32 +0200

    gtest added

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Michael Kotor on 2021-03-13.
+//
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ test/main_test.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Michael Kotor on 2021-03-13.
+//
+
+#ifndef MEMORY_MANAGEMENT_MAIN_TEST_H
+#define MEMORY_MANAGEMENT_MAIN_TEST_H
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+TEST(sample_test_case, sample_test)
+{
+    EXPECT_EQ(1, 1);
+}
+
+#endif //MEMORY_MANAGEMENT_MAIN_TEST_H
